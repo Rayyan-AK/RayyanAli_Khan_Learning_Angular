06f3355291b6815236a6236d2c6e9d3995f51d41
Rayyan <RK1020@myscc.ca>
2025-09-12 19:08:00 -0400

    Created starter Angular file and completed Assignment 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('RayyanAli-Khan-Learning-Angular');
+
+  studentName:string = 'Rayyan Ali Khan';
+  courseName:string = 'JavaScript Frameworks - Fall 2025';
 }
