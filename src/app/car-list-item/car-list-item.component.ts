4703e6ade58fa8730d4a7e6081d7f8e39b16538e
Rayyan-AK <RK1020@myscc.ca>
2025-09-30 15:25:52 -0400

    Updated ContentListItem - Displayed name/title in h1 - Applied 2px border styling within component

--- src/app/car-list-item/car-list-item.component.ts
+++ src/app/car-list-item/car-list-item.component.ts
@@ -1,11 +1,14 @@
 import {Component, Input} from '@angular/core';
 import {Car} from '../Shared/Models/car';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-car-list-item',
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './car-list-item.component.html',
-  styleUrl: './car-list-item.component.css'
+  styleUrls: ['./car-list-item.component.css']
 })
 export class CarListItemComponent {
   @Input() car!: Car;
