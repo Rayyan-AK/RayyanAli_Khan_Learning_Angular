277c4dcaba43620d63dc64c761991811fc2edc56
Rayyan-AK <RK1020@myscc.ca>
2025-09-30 15:43:48 -0400

    Update ContentList to use *ngFor and add alternating row colors
    - Replaced multiple ContentListItem tags with a single *ngFor loop
    - Applied alternating background colors (even: pink, odd: teal) inside ContentListItem

--- src/app/car-list/car-list.component.ts
+++ src/app/car-list/car-list.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import {Car} from '../Shared/Models/car';
 import {CarListItemComponent} from '../car-list-item/car-list-item.component';
+import {NgForOf, NgIf} from '@angular/common';
 
 
 @Component({
   selector: 'app-car-list',
   imports: [
-    CarListItemComponent
+    CarListItemComponent,
+    NgIf,
+    NgForOf
   ],
   templateUrl: './car-list.component.html',
   styleUrl: './car-list.component.css'
