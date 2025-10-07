c17126bb16428d2175bf1fd3d096ab3b4c0f6d7d
Rayyan-AK <RK1020@myscc.ca>
2025-10-07 12:32:00 -0400

    - Added CarService with CRUD methods using mock-content data

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -1,7 +1,7 @@
-import {Injectable} from '@angular/core';
-import {Observable, of} from 'rxjs';
-import {Car} from '../Shared/Models/car';
-import {CarListComponent} from '../data/mock-content'
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Car } from '../Shared/Models/car';
+import { cars } from '../data/mock-content';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +11,33 @@ export class CarService {
   constructor() {}
 
   getCars(): Observable<Car[]> {
-    return of(CarListComponent.cars);
+    return of(cars);
+  }
+
+  getCarByVin(vin: string): Observable<Car | undefined> {
+    const car = cars.find(c => c.vin === vin);
+    return of(car);
+  }
+
+  addCar(newCar: Car): Observable<Car[]> {
+    cars.push(newCar);
+    return of(cars);
+  }
+
+  updateCar(updatedCar: Car): Observable<Car[]> {
+    const index = cars.findIndex(c => c.vin === updatedCar.vin);
+    if (index !== -1) {
+      cars[index] = updatedCar;
+    }
+    return of(cars);
+  }
+
+  deleteCar(vin: string): Observable<Car | undefined> {
+    const index = cars.findIndex(c => c.vin === vin);
+    if (index !== -1) {
+      const removed = cars.splice(index, 1)[0];
+      return of(removed);
+    }
+    return of(undefined);
   }
 }
