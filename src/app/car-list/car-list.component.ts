54ef5361670fed2abaf9fa4ef783b1c47d14ffd9
Rayyan-AK <RK1020@myscc.ca>
2025-10-06 20:01:04 -0400

    - Moved IContent array into its own file - Created a service in its own service directory

--- src/app/car-list/car-list.component.ts
+++ src/app/car-list/car-list.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {Car} from '../Shared/Models/car';
 import {CarListItemComponent} from '../car-list-item/car-list-item.component';
 import {NgForOf, NgIf} from '@angular/common';
 
@@ -14,13 +13,7 @@ import {NgForOf, NgIf} from '@angular/common';
   templateUrl: './car-list.component.html',
   styleUrl: './car-list.component.css'
 })
+
 export class CarListComponent {
-  cars: Car[] = [
-    {vin: '1HGCM82633A004352', make: 'Honda', model: 'Civic', year: 2020, color: 'Black', image: 'cars/HondaCivic.png'},
-    {vin: '2C3KA53G76H123456', make: 'Chrysler', model: '300', year: 2018, image: 'cars/Chrysler.png'},
-    {vin: '3FAHP0HA8AR123789', make: 'Ford', model: 'Fusion', year: 2021, color: 'Red', image: 'cars/FordFusion.png'},
-    {vin: '1N4AL11D75C109876', make: 'Nissan', model: 'Altima', year: 2019, color: 'Black', image: 'cars/NissanAltima.png'},
-    {vin: '5YJSA1E26HF123654', make: 'Tesla', model: 'Model S', year: 2022, image: 'cars/TeslaS.png'},
-    {vin: 'JH4KA8270MC123321', make: 'Acura', model: 'Legend', year: 1995, color: 'White', image: 'cars/AcuraLegend.png'}
-  ];
+
 }
