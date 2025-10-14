1d087762192f5ef36d097f2b54fa430c458a65b6
Rayyan-AK <RK1020@myscc.ca>
2025-10-14 15:37:56 -0400

    - Defined a list of routes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import {Routes} from '@angular/router';
+import {CarListComponent} from './app/car-list/car-list.component';
+import {CarListItemComponent} from './app/car-list-item/car-list-item.component';
 
+export const routes: Routes = [
+  { path: '', redirectTo: '/cars', pathMatch: 'full' },
+  { path: 'cars', component: CarListComponent },
+  { path: 'cars/:id', component: CarListItemComponent },
+  { path: '**', redirectTo: '/cars' }
+];
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
