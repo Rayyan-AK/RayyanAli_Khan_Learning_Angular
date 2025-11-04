a93710d95912a29eeeb13a111e689e7fe7ee296a
Rayyan-AK <RK1020@myscc.ca>
2025-11-04 08:43:52 -0500

    Updated CarService to work with HTTP requests

--- src/app/Services/car.service.ts
+++ src/app/Services/car.service.ts
@@ -1,44 +1,59 @@
 import { Injectable } from '@angular/core';
-import {Observable, of} from 'rxjs';
+import {catchError, Observable, of, throwError} from 'rxjs';
 import {Car} from '../Shared/Models/car';
 import {cars} from '../data/mock-content';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarService {
 
-  constructor() {}
+  private apiUrl = 'api/cars';
+  private carList: Car[] = cars;
+  constructor(private http: HttpClient) {}
 
   getCars(): Observable<Car[]> {
-    return of(cars);
+    // return of(cars);
+    return this.http.get<Car[]>(this.apiUrl).pipe(catchError(this.handleError));
   }
 
   getCarByVin(vin: string): Observable<Car | undefined> {
-    const car = cars.find(c => c.vin === vin);
-    return of(car);
+    // const car = cars.find(c => c.vin === vin);
+    // return of(car);
+    return this.http.get<Car>(`${this.apiUrl}/${vin}`).pipe(catchError(this.handleError));
   }
 
-  addCar(newCar: Car): Observable<Car[]> {
-    cars.unshift(newCar);
-    return of(cars);
+  addCar(newCar: Car): Observable<Car> {
+    // cars.unshift(newCar);
+    // return of(cars);
+    return this.http.post<Car>(this.apiUrl, newCar).pipe(catchError(this.handleError));
   }
 
-  updateCar(updatedCar: Car): Observable<Car[]> {
-    const index = cars.findIndex(c => c.vin === updatedCar.vin);
-    if (index !== -1) {
-      updatedCar.image = updatedCar.image || cars[index].image;
-      cars[index] = updatedCar;
-    }
-    return of(cars);
+  updateCar(updatedCar: Car): Observable<Car | undefined> {
+    // const index = cars.findIndex(c => c.vin === updatedCar.vin);
+    // if (index !== -1) {
+    //   updatedCar.image = updatedCar.image || cars[index].image;
+    //   cars[index] = updatedCar;
+    // }
+    // return of(cars);
+    const url = `${this.apiUrl}/${updatedCar.vin}`;
+    return this.http.put<Car>(url, updatedCar).pipe(catchError(this.handleError));
   }
 
-  deleteCar(vin: string): Observable<Car | undefined> {
-    const index = cars.findIndex(c => c.vin === vin);
-    if (index !== -1) {
-      const removed = cars.splice(index, 1)[0];
-      return of(removed);
-    }
-    return of(undefined);
+  deleteCar(vin: string): Observable<{}> {
+    // const index = cars.findIndex(c => c.vin === vin);
+    // if (index !== -1) {
+    //   const removed = cars.splice(index, 1)[0];
+    //   return of(removed);
+    // }
+    // return of(undefined);
+    const url = `${this.apiUrl}/${vin}`;
+    return this.http.delete(url).pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.error('API error:', error);
+    return throwError(() => new Error('Server error, please try again.'));
   }
 }
