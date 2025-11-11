199ff43afaade423028da26bcee726b12c269b99
Rayyan-AK <RK1020@myscc.ca>
2025-11-10 22:13:19 -0500

    Converting SPA to Lazy Loading
    - Eagerly load default route
    - Lazy load the rest

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,17 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import {provideRouter, Routes} from '@angular/router';
 import {CarListComponent} from './app/car-list/car-list.component';
-import {ModifyCarComponent} from './app/modify-car/modify-car.component';
-import {PageNotFoundComponent} from './app/page-not-found/page-not-found.component';
-import {CarDetailComponent} from './app/car-detail/car-detail.component';
-
 export const routes: Routes = [
   { path: '', redirectTo: '/cars', pathMatch: 'full' },
-  { path: 'cars', component: CarListComponent },
-  { path: 'cars/:vin', component: CarDetailComponent},
-  { path: 'modify-car', component: ModifyCarComponent },
-  { path: 'modify-car/:vin', component: ModifyCarComponent },
-  { path: '**', component: PageNotFoundComponent }
+  { path: 'cars', component: CarListComponent }, //eagerly
+  // { path: 'cars/:vin', component: CarDetailComponent},
+  { path: 'cars/:vin', loadComponent: ()=> import('./app/car-detail/car-detail.component').then(m => m.CarDetailComponent) },
+  // { path: 'modify-car', component: ModifyCarComponent },
+  { path: 'modify-car', loadComponent: () => import('./app/modify-car/modify-car.component').then(m => m.ModifyCarComponent) },
+  // { path: 'modify-car/:vin', component: ModifyCarComponent },
+  { path: 'modify-car/:vin', loadComponent: () => import('./app/modify-car/modify-car.component').then(m => m.ModifyCarComponent) },
+  // { path: '**', component: PageNotFoundComponent }
+  { path: '**', loadComponent: () => import('./app/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent) },
 ];
 bootstrapApplication(App, {
   providers: [provideRouter(routes)]
